6cc8b72d8ada9cf006d0e9a6fdc780fd3bee2298
hichamrox <h.echarifchefchaouni@ecole-ipssi.ne>
2022-05-03 16:55:20 +0200

    index

--- public/index.php
+++ public/index.php
@@ -7,4 +7,24 @@ require ROOT ."/vendor/autoload.php";
 
 // Chargement du routeur
 require ROOT ."/Core/Router/Router.php";
-Router::router(); 
+
+if (isset($_SERVER["PATH_INFO"])) {
+    $path = explode("/",$_SERVER["PATH_INFO"]);
+    if (isset($path[3])) {
+        $controllerName = "App\Controller\\". ucfirst($path[3]) . "Controller";
+        $controller = new $controllerName();
+
+        if (isset($path[4]) && is_numeric($path[4])) {
+            $controller->single($path[4]);
+        } elseif (isset($path[4]) && is_string($path[4])) {
+            $method = $path[4];
+            $controller->$method();
+        } else {
+            $controller->index();
+        }
+
+    }
+
+} else {
+    // TODO: Error message
+}
