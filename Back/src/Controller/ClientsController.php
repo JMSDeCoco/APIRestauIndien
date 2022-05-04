70e7b1101fcc41ab7de106ae55543b6779e3be90
hichamrox <h.echarifchefchaouni@ecole-ipssi.ne>
2022-05-05 01:31:08 +0200

    client

--- Back/src/Controller/ClientsController.php
+++ Back/src/Controller/ClientsController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Model\ClientsModel;
 use Core\Controller\DefaultController;
+use App\Security\JwTokenSecurity;
 
 class ClientsController extends DefaultController {
 
@@ -22,6 +23,38 @@ class ClientsController extends DefaultController {
      * @param array $client
      * @return void
      */
+
+    public function signup (array $client): void
+    {
+        if (isset($client["nom"], $client["prenom"], $client["email"], $client["phone"], $client["password"])) {
+
+            $client['password'] = password_hash($client['password'], PASSWORD_DEFAULT);
+            $client['roles'] = json_encode([]);
+
+            $lastId = $this->model->save($client);
+
+            $this->jsonResponse($this->model->find($lastId));
+        }
+    }
+
+    public function login (array $clientData): void
+    {
+        // TODO: Vérifier que $clientData contient un email et un password sinon on retourne une erreur
+        $client = $this->model->getclientByEmail($clientData['email']);
+
+        if ($client) {
+            if (password_verify($clientData['password'], $client->getPassword())) {
+
+                $this->jsonResponse((new JwTokenSecurity)->generateToken($client->jsonSerialize()));
+
+            } else {
+                $this->jsonResponse("Mot de passe incorrect", 400);
+            }
+        } else {
+            $this->jsonResponse("Cet utilisateur n'est pas inscrit, veuillez vous inscrire", 400);
+        }
+    }
+
     public function save (array $client): void
     {
         // Génère l'apikey
